60a8c5225794ce9e105cfd1f1e112a395cea3cd8
tenisme <iamchoma@gmail.com>
2020-07-27 18:16:11 +0900

    update 200727

--- MySQL_sql/nodejs 관련/200724.sql
+++ MySQL_sql/nodejs 관련/200724.sql
@@ -1,5 +1,5 @@
 -- 200724
--- use my_test;
+use my_test;
 
 -- create table movie (
 -- 	id int not null auto_increment primary key,
@@ -13,4 +13,19 @@
 
 select * from movie;
 
-select * from movie where title like '%to%' limit 0, 25;
+select * from movie where title like '%%' limit 0, 25;
+
+select * from movie where title like '%%' order by year asc limit 0, 25;
+
+desc movie;
+
+# 200727 월요일
+
+select * from token;
+
+select * from user;
+
+
+
+select * from user as u join token as t on u.id = t.user_id;
+delete from user where id = (select u.id from user as u join token as t on u.id = t.user_id where u.id = 1);
